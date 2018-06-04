09aa8198deb2f2e8ce205e796b6096210876aa77
Euan <bell_designs@outlook.com>
2018-06-04 10:33:14 +0100

    populated Dinosaur Super

--- JurrasicParkJavaProject/src/main/java/Dinosaurs/Dinosaur.java
+++ JurrasicParkJavaProject/src/main/java/Dinosaurs/Dinosaur.java
@@ -1,4 +1,36 @@
 package Dinosaurs;
 
+import java.util.ArrayList;
+
 public class Dinosaur {
+
+    private String name;
+    private String species;
+    private double weight;
+    private ArrayList<Food> Stomach;
+
+    public Dinosaur(String name, String species, Double weight){
+
+        this.name = name;
+        this.species = species;
+        this.weight = weight;
+        this.Food = new ArrayList<>();
+    }
+
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getSpecies() {
+        return this.species;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public ArrayList<Food> getFood() {
+        return this.Food;
+    }
 }
