e805fa64a627307aad6a0436f20af692d0e91092
Euan <bell_designs@outlook.com>
2018-06-05 09:59:47 +0100

    refactored diplodocus and veleciraptor to include attackPower

--- JurrasicParkJavaProject/src/main/java/Dinosaurs/velociraptor.java
+++ JurrasicParkJavaProject/src/main/java/Dinosaurs/velociraptor.java
@@ -3,7 +3,7 @@ package Dinosaurs;
 public class Velociraptor extends Dinosaur {
 
     public Velociraptor(String name, String species, double weight, int attackPower){
-        super(name, species, weight, );
+        super(name, species, weight, attackPower);
 
     }
 
