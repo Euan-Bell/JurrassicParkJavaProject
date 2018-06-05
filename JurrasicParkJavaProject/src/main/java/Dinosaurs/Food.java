c1fe39d285d9a1700354033bba1db7d9b4fa7fba
Euan <bell_designs@outlook.com>
2018-06-05 12:10:18 +0100

    added tests to foodtest class

--- JurrasicParkJavaProject/src/main/java/Dinosaurs/Food.java
+++ JurrasicParkJavaProject/src/main/java/Dinosaurs/Food.java
@@ -1,6 +1,24 @@
 package Dinosaurs;
 
-public class Food{}
+public class Food{
+
+    private String foodname;
+    protected DietType dietType;
+
+    public Food (String foodname, DietType dietType){
+
+        this.foodname = foodname;
+        this.dietType = dietType;
+    }
+
+    public String getFoodName() {
+        return this.foodname;
+    }
+
+    public DietType getFoodType() {
+        return this.dietType;
+    }
+}
 
 
 
--- JurrasicParkJavaProject/src/test/java/DinosaursTest/foodTest.java
+++ JurrasicParkJavaProject/src/test/java/DinosaursTest/foodTest.java
@@ -1,4 +1,28 @@
 package DinosaursTest;
 
+import Dinosaurs.DietType;
+import Dinosaurs.Food;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class foodTest {
+
+   Food food;
+
+   @Before
+    public void before(){
+       food = new Food("Plantlife", DietType.HERBIVORE);
+   }
+
+   @Test
+    public void getFoodName(){
+       assertEquals("Plantlife",food.getFoodName());
+   }
+
+   @Test
+    public void getFoodType(){
+       assertEquals(DietType.HERBIVORE, food.getFoodType());
+   }
 }
