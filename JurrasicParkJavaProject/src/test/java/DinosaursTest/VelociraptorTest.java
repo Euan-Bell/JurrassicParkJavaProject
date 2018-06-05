1305a1b5e054149e154cf8838fbd7cf4915d087c
Euan <bell_designs@outlook.com>
2018-06-05 16:20:34 +0100

    added all food tests to Velociraptor

--- JurrasicParkJavaProject/src/test/java/DinosaursTest/VelociraptorTest.java
+++ JurrasicParkJavaProject/src/test/java/DinosaursTest/VelociraptorTest.java
@@ -15,11 +15,15 @@ public class VelociraptorTest {
     Velociraptor velociraptor;
     ArrayList<Food> stomach;
     Food food;
+    Food food1;
+    Food food2;
 
     @Before
     public void before() {
         velociraptor = new Velociraptor("Iain", "Velociraptor", 00.15, 30, DietType.CARNIVORE);
         food = new Food("CowChunks", DietType.CARNIVORE);
+        food1 = new Food("PlantLife", DietType.HERBIVORE);
+        food2 = new Food("Chicken StirFry", DietType.OMNIVORE);
     }
 
     @Test
@@ -48,10 +52,24 @@ public class VelociraptorTest {
     }
 
     @Test
-    public void canFeedDinosaur(){
+    public void canHaveVelociraptorStickToDietTYPE(){
         velociraptor.feedDinosaur(food);
         assertEquals(1, velociraptor.canBeFed());
     }
+
+    @Test
+    public void canHaveVelociraptorUnableToEatAnythingOtherThanDietType(){
+        velociraptor.feedDinosaur(food1);
+        assertEquals(0, velociraptor.canBeFed());
+    }
+
+    @Test
+    public void canHaveVelociraptorUnableToEatAnythingOtherThanDietType2(){
+        velociraptor.feedDinosaur(food2);
+        assertEquals(0, velociraptor.canBeFed());
+    }
+
+
 
     @Test
     public void canGetAttackPower(){
