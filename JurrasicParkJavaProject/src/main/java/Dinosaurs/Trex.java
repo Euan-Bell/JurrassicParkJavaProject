975d2ccf96a0dc813d0c0c5674b28341ca525b97
Euan <bell_designs@outlook.com>
2018-06-06 10:04:05 +0100

    created Trex Class

---
+++ JurrasicParkJavaProject/src/main/java/Dinosaurs/Trex.java
@@ -0,0 +1,8 @@
+package Dinosaurs;
+
+public class Trex extends Dinosaur {
+
+    public Trex(String name, String species, double weight, int attackPower, Dinosaurs.DietType dietType){
+        super(name, species, weight, attackPower, dietType);
+    }
+}
